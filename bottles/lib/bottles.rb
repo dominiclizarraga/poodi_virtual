e83f9a10807426cb667ef009f039c69162274a7e
dominiclizarraga <dominiclizarraga@hotmail.com>
2025-10-21 19:00:42 -0600

    Add self.for method and moved the factory out of verse to BottleNumber

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,16 +8,24 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = bottle_number_for(number)
-    next_bottle_number = bottle_number_for(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
+    # next_bottle_number = BottleNumber.for(bottle_number.successor)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number} of beer on the wall.\n"
+    "#{bottle_number.successor} of beer on the wall.\n"
+  end
+end
+
+class BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
   end
 
-  def bottle_number_for(number)
+  def self.for(number)
     case number
     when 0
       BottleNumber0
@@ -26,14 +34,6 @@ class Bottles
     else
       BottleNumber
     end.new(number)
-  end
-end
-
-
-class BottleNumber
-  attr_reader :number
-  def initialize(number)
-    @number = number
   end
 
   def to_s
@@ -57,7 +57,7 @@ class BottleNumber
   end
 
   def successor
-    number - 1
+    BottleNumber.for(number - 1)
   end
 end
 
@@ -71,7 +71,7 @@ class BottleNumber0 < BottleNumber
   end
 
   def successor
-    99
+    BottleNumber.for(99)
   end
 end
 
